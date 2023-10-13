d66a2b98e53a0fb9268eb8bca597e34a85ebcb01
Polo4722valentina <vergelesvalentina07@gmail.com>
2023-10-13 17:34:41 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 
 
 def quadratic_solver(a, b, c):
-    ...
+    discriminant = b**2 - 4 * a * c
+
+    if discriminant > 0:
+        root1 = ( -b + math.sqrt(discriminant)) / (2*a)
+        root2 = ( -b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root
+    else:
+        return "Корені відсутні"
 
 
 
