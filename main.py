e9e5959f74cf80ae372946c8b8ce232cc9f23c62
mariyyya12 <mariya-zinchenko@ukr.net>
2023-10-13 17:04:29 +0300

    Solution

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 
 
 def quadratic_solver(a, b, c):
-    ...
+    # Обчислюємо дискримінант
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        # Два дійсних корені
+        x1 = ( -b + (discriminant)**0.5) / (2*a)
+        x2 = ( -b - (discriminant)**0.5) / (2**a)
+        return (x1, x2)
+    elif discriminant == 0:
+        # Один дійсний корніь (корені співпадають)
+        x = -b / (2*a)
+        return x
+    else:
+        # "Корені відсутні  ( корені комплексні)"
+        return "Корені відсутні  ( корені комплексні)"
 
 
 
