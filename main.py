10d4f859a6349742d09303534ecefeb381706621
“podliyPadonak” <supercell0boombeach@gmail.com>
2023-10-13 17:21:29 +0300

    develop

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
-
 def quadratic_solver(a, b, c):
-    ...
-
-
-
+    discriminant = b**2 - 4 * a * c
+    discr_pow = pow(discriminant,(1/2))
+    x1 = (-b-discr_pow)/(2*a)
+    x2 = (-b+discr_pow)/(2*a)
+    return [x1, x2]
 
 # Приклад використання функції
 a = 1
