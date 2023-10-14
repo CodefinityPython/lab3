0bdbc456ee9dac9fe539392042bcfc249d82d5c1
Oleksandr-27 <sashakrivetka2004@icloud.com>
2023-10-14 12:59:08 +0300

    solution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 a = int(input())
 b = int(input())
 c = int(input())
-def quadratic_solver(a, b, c):
-    print(b ** 2 + 4 * a * c)
-quadratic_solver(a = a, b = b, c = c)
+def quadratic_solver( a, b, c):
+    print(b ** 2 - (4 * a * c))
+quadratic_solver( a = a, b = b, c = c)
