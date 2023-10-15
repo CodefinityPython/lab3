15f54420c612b2afe27c7dcbd53da2f8ea210599
“rillkaua” <rillkatvua@gmail.com>
2023-10-15 11:06:17 +0300

    solution

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from math import sqrt
 
 
 def quadratic_solver(a, b, c):
-    ...
+    d = b**2 - 4 * a * c
+
+    if d == 0:
+        x = (-b + sqrt(d)) / (2 * a)
+        return x
+    elif d > 0:
+        x1 = (-b + sqrt(d)) / (2 * a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        return [x1, x2]
+    else:
+        return None
 
 
 
