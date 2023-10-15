c59dc39409be88a019331201e6884fe0154af19b
Mykhailo <meshagromov2012@gmail.com>
2023-10-16 01:15:03 +0300

    solution

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-
+import math
 
 def quadratic_solver(a, b, c):
-    ...
-
-
-
-
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        result1 = (-b + math.sqrt(discriminant)) / (2*a)
+        result2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return result1, result2
+    elif discriminant == 0:
+        result = -b / (2*a)
+        return result
+    else:
+        return "No real solver"
 # Приклад використання функції
 a = 1
 b = -3
