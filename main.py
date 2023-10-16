febc2608e67f69e2f61f36ca00290a06703c480d
VadymRybachek <ryb.vadymchek@gmail.com>
2023-10-17 00:50:22 +0300

    Solution

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 
 
 def quadratic_solver(a, b, c):
-    ...
+
+    discriminant = b ** 2 - 4 * a * c
+
+    if discriminant > 0:
+
+        sqrt_discriminant = discriminant ** 0.5
+        x1 = (-b + sqrt_discriminant) / (2 * a)
+        x2 = (-b - sqrt_discriminant) / (2 * a)
+        return x1, x2
+    elif discriminant == 0:
+
+        x1 = -b / (2 * a)
+        return x1
+    else:
+
+        return ("Немає коренів")
 
 
-
-
-# Приклад використання функції
 a = 1
 b = -3
 c = 2
