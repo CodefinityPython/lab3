dcaf3cbf97efad44bf714cadfbbcf0a19a0b1fb1
aleksandrmazinskas <mazinskas04@gmail.com>
2023-10-17 20:14:23 +0300

    Solution

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 
 
 def quadratic_solver(a, b, c):
-    ...
+    diskr = b ** 2 - 4 * a * c
 
+    if diskr > 0 :
 
+        x1 = (-b + 2 ** 1/2) / (2 * a)
+        x2 = (-b - 2 ** 1/2) / (2 * a)
+        print('Корені дорівнюють:')
+        return x1 , x2
 
+    elif diskr == 0 :
+        x = -b / 2 * a
+        return x
+
+    else:
+        return 'Коренів немає'
 
 # Приклад використання функції
 a = 1
