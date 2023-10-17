624a23c29351bdcbcc8e196d9feb08d530440233
2switch2 <graznoy842@gmail.com>
2023-10-17 12:09:49 +0300

    math

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+import math
 
 
-def quadratic_solver(a, b, c):
-    ...
+def find_quadratic_roots(a, b, c):
+    # Обчислюємо дискримінант
+    discriminant = b ** 2 - 4 * a * c
 
+    # Якщо дискримінант менше 0, немає реальних коренів
+    if discriminant < 0:
+        return "Немає реальних коренів"
 
+    # Якщо дискримінант дорівнює 0, є один корінь
+    elif discriminant == 0:
+        root = -b / (2 * a)
+        return root
+
+    # Якщо дискримінант більше 0, є два корені
+    else:
+        root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        return root1, root2
 
 
 # Приклад використання функції
-a = 1
+a = 2
 b = -3
-c = 2
-result = quadratic_solver(a, b, c)
-print(result)
+c = 1
+
+roots = find_quadratic_roots(a, b, c)
+print(roots)
