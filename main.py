ad9c19caa90283eb42a3a599c74f20643a0a5b0c
Alasteir <seyakto@gmail.com>
2023-10-17 12:45:58 +0300

    Solution

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 
-
+import math
 def quadratic_solver(a, b, c):
-    ...
-
-
-
-
+    d = b**2-4*a*c
+    if d > 0 :
+     x_1 = round(-b + math.sqrt(d)/(2*a), 1)
+     x_2 = round(-b - math.sqrt(d)/(2*a), 1) 
+     return(f"Roots: {x_1}:{x_2}. ")
+    elif d==0:
+       x = round(-b /(2*a), 1)
+       return(f"Roots: {x}. ")
+    else:
+       return("No Root")   
 # Приклад використання функції
 a = 1
 b = -3
