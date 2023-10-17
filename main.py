c46aa64db4e7b7bca89ff9153a2bf374a5f117cd
“YelyzavetaFedenko” <yelyzaveta.fedenko@hebron-academy.com>
2023-10-17 12:32:17 +0300

    Correction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 
 def quadratic_solver(a, b, c):
     disc = b**2-4*a*c
-    if disc > 0:
+    if disc != 0:
         x1 = round((-b + math.sqrt(disc)) / (2 * a), 1)
         x2 = round((-b - math.sqrt(disc)) / (2 * a), 1)
         return f"Found two equation roots: {x1}, {x2}."
