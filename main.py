29da929f465e6a3bc2c2a2512aaf3c10538c7859
ivanpylypchuk <vanyyaapilipchyk@gmail.com>
2023-10-17 19:07:48 +0300

    Solution

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-
-
 def quadratic_solver(a, b, c):
     ...
-
+    discriminant = b**2 - 4*a*c
+    if discriminant > 0:
+        root1 = (-b + discriminant**0.5) / (2*a)
+        root2 = (-b - discriminant**0.5) / (2*a)
+        return (root1, root2)
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return  (root,)
+    else:
+        return None
 
 
 
