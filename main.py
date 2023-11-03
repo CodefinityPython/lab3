425ec65d74085af2cc15fd8de5dbbb78956b8fa6
“HannaMotorova” <phantasmagooorical@gmail.com>
2023-11-03 18:15:54 +0200

    Solution

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 
-
 def quadratic_solver(a, b, c):
-    ...
 
+    D = b** 2 - 4 * a * c
+    print("Discriminant = ", D)
 
-
+    if D > 0:
+        print("There are two real solutions:")
+        x1 = (-b + D ** 0.5) / (2 * a)
+        x2 = (-b - D ** 0.5) / (2 * a)
+        print(f"x1 = {x1}, x2 = {x2}")
+    elif D == 0:
+        print("There is one real solution:")
+        x = -b / (2*a)
+        print(f"x = {x}")
+    else:
+        print("There are no real solutions!")
 
 # Приклад використання функції
-a = 1
-b = -3
-c = 2
-result = quadratic_solver(a, b, c)
+result = quadratic_solver(2, -7, 6)
 print(result)
